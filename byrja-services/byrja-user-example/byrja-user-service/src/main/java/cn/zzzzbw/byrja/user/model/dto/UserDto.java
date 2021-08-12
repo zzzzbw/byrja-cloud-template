fc862b91d9acc11d3e9a40abb998d383c5b8b052
zzzzbw <zzzzbw@gmail.com>
2021-08-12 22:30:32 +0800

    feat: maven打包功能

---
+++ byrja-services/byrja-user-example/byrja-user-service/src/main/java/cn/zzzzbw/byrja/user/model/dto/UserDto.java
@@ -0,0 +1,15 @@
+package cn.zzzzbw.byrja.user.model.dto;
+
+import lombok.Data;
+
+/**
+ * @author by ZHANGBOWEN469
+ * @since 2021/8/11 15:23
+ */
+@Data
+public class UserDto {
+
+    private String name;
+
+    private int age;
+}
